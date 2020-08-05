1b36c05da74cf383bbbb7cc5d426e577315cdc61
alisyn-schilling <aeschilling.87@gmail.com>
2020-08-05 11:04:56 -0400

    added map to admin profile page and made the automatic lat long input work

--- src/main/resources/static/js/autocomplete.js
+++ src/main/resources/static/js/autocomplete.js
@@ -7,20 +7,52 @@ function initAutocomplete() {
     );
     // Avoid paying for data that you don't need by restricting the set of
     // place fields that are returned to just the address components.
-    autocomplete.setFields(["geometry"]);
+    autocomplete.setFields(["address_component", "geometry"]);
 
     // When the user selects an address from the drop-down, populate the
     // address fields in the form.
-    autocomplete.addListener("place_changed", fillInAddress);
+    autocomplete.addListener("place_changed", fillInCoordinates);
+    initMap();
 }
 
 function fillInCoordinates() {
     // Get the place details from the autocomplete object.
-    var place = autocomplete.getPlace();
+    const place = autocomplete.getPlace();
+    const latField = document.getElementById("latitude");
+    const lngField = document.getElementById("longitude");
+    latField.value = "" + place.geometry.location.lat();
+    lngField.value = "" + place.geometry.location.lng()
 
     // Get each component of the address from the place details,
     // and then fill-in the corresponding field on the form.
 
 
     // initMap(place.geometry.location.lat(), place.geometry.location.lng());
+}
+
+function geolocate() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            const geolocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+            };
+            const circle = new google.maps.Circle({
+                center: geolocation,
+                radius: position.coords.accuracy,
+            });
+            autocomplete.setBounds(circle.getBounds());
+        });
+    }
+}
+
+const mapDiv = document.getElementById("map")
+
+function initMap () {
+    const location = { lat: latitude, lng: longitude };
+    const map = new google.maps.Map(mapDiv, {
+        zoom: 15,
+        center: location,
+    });
+    const marker = new google.maps.Marker({ position: location, map: map });
 }
--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -2,7 +2,6 @@ const mapDiv = document.getElementById("map")
 
 function initMap () {
     const location = { lat: latitude, lng: longitude };
-    console.log(location)
     const map = new google.maps.Map(mapDiv, {
         zoom: 15,
         center: location,
